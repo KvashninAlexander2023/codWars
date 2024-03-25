9fd65ec12a8164dbf82adcef0d244f81611a5a4b
KvashninAlexander2023 <study_python@mail.ru>
2024-03-25 07:00:43 +0500

    new task 7kyu

---
+++ codWar/7 kyu/Categorize New Member.js
@@ -0,0 +1,24 @@
+//Input
+//Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+//Output
+//Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+//Example
+//input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+//output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+function openOrSenior(data){
+  // ...
+}
+
+
+
+
+
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]))//,['Open', 
+console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]))//,['Open', 'Open', 'Open', 'Open'])
+
+console.log(openOrSenior([[59, 12],[55,-1],[12, -2],[12, 12]]))//,['Senior', 'Open', 'Open', 'Open']
+
+
