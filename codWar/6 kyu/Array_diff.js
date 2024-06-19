80ef312071f3f7e2f144605cec59c3346ac3e470
KvashninAlexander2023 <study_python@mail.ru>
2024-06-19 21:45:06 +0500

    add 6 kyu

---
+++ codWar/6 kyu/Array_diff.js
@@ -0,0 +1,21 @@
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+
+function arrayDiff(a, b) {
+  newArr = []
+  a.forEach(el => {
+    b.includes(el) ? null : newArr.push(el)})
+  return newArr
+}
+
+console.log(arrayDiff([1,2,2,2,3],[2]));//[1,3]
+
+//можно так
+// function array_diff(a, b) {
+//   return a.filter(e => !b.includes(e));
+// }
