e3799beedae589160d845b9b072120fd40f26666
KvashninAlexander2023 <study_python@mail.ru>
2024-07-11 19:31:51 +0500

    add 8 kyu

---
+++ codWar/8 kyu/Grasshopper - Messi Goals.js
@@ -0,0 +1,9 @@
+const laLigaGoals = 43
+const championsLeagueGoals = 10
+const copaDelReyGoals = 5
+
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+function getTotalGoals(){
+  return totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+}
