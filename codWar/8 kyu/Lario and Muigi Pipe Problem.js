750731ff098a2f4f782a0628554fe7afd5de0120
KvashninAlexander2023 <study_python@mail.ru>
2024-07-11 19:58:44 +0500

    add 8 kyu

---
+++ codWar/8 kyu/Lario and Muigi Pipe Problem.js
@@ -0,0 +1,10 @@
+function pipeFix(numbers) {
+  let newArr = [];
+  for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+    console.log("s");
+    newArr.push(i);
+  }
+  return newArr;
+}
+
+console.log(pipeFix([1, 2, 3, 5, 6, 8, 9]));
