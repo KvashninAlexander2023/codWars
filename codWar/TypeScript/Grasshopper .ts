37d943552385cbe921a381eb7a32d2b0e9649ed0
KvashninAlexander2023 <study_python@mail.ru>
2025-01-12 18:22:55 +0500

    add 8 kyu typescript

---
+++ codWar/TypeScript/Grasshopper .ts
@@ -0,0 +1,8 @@
+// исправить код
+// export function main [verb: string, noun: string]: string
+//   return verb + noun
+// }
+
+export function main (verb: string, noun: string): string {
+  return verb + noun
+}
